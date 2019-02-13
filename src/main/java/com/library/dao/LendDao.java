f83f399e5db009bc83011b4ebeefa6ce2a1541e5
zhanghuanhao <254892464@qq.com>
2019-02-13 14:56:42 +0800

    Add lend

--- src/main/java/com/library/dao/LendDao.java
+++ src/main/java/com/library/dao/LendDao.java
@@ -5,7 +5,6 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,24 +13,15 @@ import java.util.Map;
 @Repository
 public class LendDao {
 
-    private final static String NAMESPACE = "com.library.dao.LendDao.";
-    private final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
     @Resource
     private SqlSessionTemplate sqlSessionTemplate;
 
-    private final static String RETURN_BOOK_ONE_SQL = "UPDATE lend_list SET back_date = ? WHERE book_id = ? AND reader_id = ? AND back_date is NULL";
-    private final static String RETURN_BOOK_TWO_SQL = "UPDATE book_info SET number = number + 1 WHERE book_id = ? ";
-    private final static String LEND_BOOK_ONE_SQL = "INSERT INTO lend_list VALUES (NULL , ? , ? , ? , NULL)";
-    private final static String LEND_BOOK_TWO_SQL = "UPDATE book_info SET number = number - 1 WHERE book_id = ? ";
-    private final static String LEND_LIST_SQL = "SELECT * FROM lend_list";
-    private final static String MY_LEND_LIST_SQL = "SELECT * FROM lend_list WHERE reader_id = ? ";
-    private final static String DELETE_LEND_SQL = "DELETE FROM lend_list WHERE ser_num = ?";
+    private final static String NAMESPACE = "com.library.dao.LendDao.";
 
     public int returnBookOne(final long book_id, long reader_id) {
         Map<String, Object> map = new HashMap<>();
         map.put("book_id", book_id);
         map.put("reader_id", reader_id);
-        //map.put("back_date", df.format(new Date()));
         return sqlSessionTemplate.update(NAMESPACE + "returnBookOne", map);
     }
 
